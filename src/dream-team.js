061bdddd0d0a674b04d0948c10cc40c926c1d673
simatyt <simatyt@gmail.com>
2020-09-22 00:08:11 +0300

    feat: implement dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function createDreamTeam(stringArray) {
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (
+    !Array.isArray(stringArray) ||
+    stringArray.length === 0 ||
+    !stringArray
+  ) return false;
+
+  const result = stringArray.filter(isNotEmptyString).map(getFirstLetter).sort().join("");
+
+  if (result) {
+    return result;
+  } else {
+    return false;
+  }
+
 };
+
+function isNotEmptyString(s) {
+  return s && typeof s === 'string' && s !== "";
+}
+
+function getFirstLetter(str) {
+  return str.trim()[0].toUpperCase();
+}
