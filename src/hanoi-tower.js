86bdd80ddaf640f773abad87ed7340f7f94da403
simatyt <simatyt@gmail.com>
2020-09-23 00:55:10 +0300

    feat: implement "hanoi-tower" task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
-const CustomError = require("../extensions/custom-error");
+function calculateHanoi(disksNumber, turnsSpeed) {
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  const turns = Math.pow(2, disksNumber) - 1;
+  const seconds = Math.floor(turns / (turnsSpeed / 3600));
+
+  return {
+    turns,
+    seconds
+  };
+}
+
+module.exports = calculateHanoi;
