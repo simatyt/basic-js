8e9878ef7bd7ebac33081d860f6fa167e80f14c7
simatyt <simatyt@gmail.com>
2020-09-23 01:24:48 +0300

    feat: imlement "carbon dating" task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+
+function dateSample(sampleActivity) {
+
+  if (typeof sampleActivity !== "string") return false;
+
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const ratio = Math.log(MODERN_ACTIVITY / sampleActivity);
+  const result = Math.ceil(ratio / k);
+  
+  if (
+    isNaN(result) ||
+    result < 0 ||
+    !Number.isFinite(result)
+  ) {
+    return false;
+  }
+  
+  return result;
+}
+
+module.exports = dateSample;
